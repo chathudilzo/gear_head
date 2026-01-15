1cb47c5ad63ec5a5eef863a6df49d3700084a3d1
chathudilzo <chathuradilshanudawaththa@gmail.com>
2026-01-15 12:03:55 +0530

    added new dashboard values

--- lib/features/dashboard/data/mock_obd_service.dart
+++ lib/features/dashboard/data/mock_obd_service.dart
@@ -3,16 +3,22 @@ import 'dart:math';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MockOBDService {
-  Stream<Map<String, double>> getVehicleDataStream() {
+  Stream<Map<String, dynamic>> getVehicleDataStream() {
     return Stream.periodic(const Duration(milliseconds: 100), (tick) {
       return {
         'rpm': 800 + (2500 * (1 + sin(tick * 0.1))),
-        'speed': 40 + (20 * (1 + cos(tick * 0.05)))
+        'speed': 40 + (20 * (1 + cos(tick * 0.05))),
+        'temp': 90 + (5 * sin(tick * 0.01)),
+        'voltage': 13.8 + (0.4 * cos(tick * 0.02)),
+        'load': 20 + (15 * (1 + sin(tick * 0.08))),
+        'fuel': 75.0 - (tick * 0.0001),
+        'oil': 88.0,
+        'status': tick % 100 == 0 ? "P0101" : "HEALTHY"
       };
     });
   }
 }
 
-final vehcileStreamProvider = StreamProvider<Map<String, double>>((ref) {
+final vehcileStreamProvider = StreamProvider<Map<String, dynamic>>((ref) {
   return MockOBDService().getVehicleDataStream();
 });
--- lib/features/dashboard/presentation/dashboard_screen.dart
+++ lib/features/dashboard/presentation/dashboard_screen.dart
@@ -22,51 +22,85 @@ class DashboardScreen extends ConsumerWidget {
         centerTitle: true,
       ),
       body: vehicleData.when(
-          data: (data) => Column(
-                children: [
-                  Center(
-                    child: SizedBox(
-                      width: 280,
-                      height: 280,
-                      child: CustomPaint(
-                        painter: GaugePainter(
-                            value: data['rpm'] ?? 0, max: 8000, label: "RPM"),
-                        child: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                (data['rpm']?.toInt()).toString(),
-                                style: TextStyle(
-                                    fontSize: 48,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.primaryAccent),
-                              ),
-                              Text(
-                                "RPM",
-                                style: TextStyle(color: AppColors.textPrimary),
-                              )
-                            ],
+          data: (data) => SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        width: 280,
+                        height: 280,
+                        child: CustomPaint(
+                          painter: GaugePainter(
+                              value: data['rpm'] ?? 0, max: 8000, label: "RPM"),
+                          child: Center(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  (data['rpm']?.toInt()).toString(),
+                                  style: TextStyle(
+                                      fontSize: 48,
+                                      fontWeight: FontWeight.bold,
+                                      color: AppColors.primaryAccent),
+                                ),
+                                Text(
+                                  "RPM",
+                                  style:
+                                      TextStyle(color: AppColors.textPrimary),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  PerformanceChart(rpmPoints: rpmPoints),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _buildStatCard("SPEED", "${data['speed']?.toInt()} km/h"),
-                      _buildStatCard("ENGINE", "STABLE")
-                    ],
-                  )
-                ],
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                        height: 180,
+                        child: PerformanceChart(rpmPoints: rpmPoints)),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    // Row(
+                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //   children: [
+                    //     _buildStatCard("SPEED", "${data['speed']?.toInt()} km/h"),
+                    //     _buildStatCard("ENGINE", "STABLE")
+                    //   ],
+                    // )
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: GridView.count(
+                        crossAxisCount: 3,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 1.1,
+                        children: [
+                          _buildMiniTile("SPEED",
+                              '${(data['speed'] ?? 0).toInt()}', "km/h"),
+                          _buildMiniTile(
+                              "TEMP", "${(data['temp'] ?? 0).toInt()}", "C"),
+                          _buildMiniTile(
+                              "BATT",
+                              "${(data["voltage"] ?? 12.6).toStringAsFixed(1)}",
+                              "V"),
+                          _buildMiniTile(
+                              "LOAD", "${(data['load'] ?? 0).toInt()}", "%"),
+                          _buildMiniTile(
+                              "OIL", "${(data['oil'] ?? 0).toInt()}", "%"),
+                          _buildMiniTile(
+                              "FUEL", "${(data['fuel'] ?? 0).toInt()}", "%"),
+                        ],
+                      ),
+                    ),
+                    _buildStatusBar(data['status'])
+                  ],
+                ),
               ),
           error: (err, stack) => Center(
                 child: Text("Error:$err"),
@@ -74,6 +108,37 @@ class DashboardScreen extends ConsumerWidget {
           loading: () => Center(
                 child: CircularProgressIndicator(),
               )),
+    );
+  }
+
+  Widget _buildStatusBar(String? status) {
+    final isHealthy = status == "HEALTHY";
+
+    return Container(
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          color: isHealthy
+              ? Colors.green.withOpacity(0.1)
+              : Colors.red.withOpacity(0.1),
+          border: Border.all(color: isHealthy ? Colors.green : Colors.red)),
+      child: Row(
+        children: [
+          Icon(
+            isHealthy ? Icons.check_circle : Icons.warning,
+            color: isHealthy ? Colors.green : Colors.green,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            isHealthy ? "SYSTEM HEALTHY" : "FAULT DETECTED:$status",
+            style: TextStyle(
+                color: isHealthy ? Colors.green : Colors.red,
+                fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 
@@ -94,6 +159,33 @@ class DashboardScreen extends ConsumerWidget {
           Text(
             value,
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMiniTile(String label, String value, String unit,
+      {Color? alertColor}) {
+    return Container(
+      decoration: BoxDecoration(
+          color: AppColors.surface,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.white)),
+      child: Column(
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 10, color: Colors.grey),
+          ),
+          Text(
+            value,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            unit,
+            style: TextStyle(
+                fontSize: 10, color: alertColor ?? AppColors.primaryAccent),
           )
         ],
       ),
